f1ea9b0d007407ad8ac449ad48ebc11a3ff56773
Renzaki <renzakithakuri@gmail.com>
2024-04-02 09:07:00 +0545

    fixing bugs occured during deployment

--- api/index.js
+++ api/index.js
@@ -31,10 +31,10 @@ app.listen(3000, () => {
 app.use("/api/auth", authRouter);
 app.use("/api/user", userRouter);
 
-app.use(express.static(path.join(__dirname,'/client/dist')))
+app.use(express.static(path.join(__dirname, '/client/dist')));
 
-app.get('*',(req,res)=>{
-  res.sendFile(path.join(__dirname,"client","dist","index.html"))
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
